e90512c1166bae2436e6f7d111d48d74b0262820
sriram <khandavillisriram44@gmail.com>
2023-06-12 07:27:10 +0000

    list-and-keys

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import UserProfile from "./components/UserProfile/index.js"
+
+const userDetails = {
+  imgUrl:'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+  name:'Rahul',
+  role:'softwareDeveloper',
+
+}
+
+const App = () => <UserProfile />
+export default App;
